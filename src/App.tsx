94ebc1aa487c8949a39b7de06c828e9658b559c8
unknown <kleiton.batista@catolica.edu.br>
2022-06-26 00:40:36 -0300

    layout inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
+import { Evento } from "./pages/Evento";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
 
 interface Lesson {
   id: string;
   title: string;
 }
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
   return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+   <div>
+      <Evento/>
+   </div>
   );
 }
 
